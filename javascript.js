949cc7fcd78b6603e62138f20f2f11d3c4c803f2
John Bateman <jbateman415@gmail.com>
2018-06-20 18:49:00 +0000

    commented out js and added anchors to index.htm

--- javascript.js
+++ javascript.js
@@ -23,6 +23,7 @@ for (var loopCounter2 = 0; loopCounter2 <fruits.length; loopCounter2++) {
 }
 */
 
+/*
 var exampleObject = {"Key1":1, "Key2":2, "Key3":3 };
 console.log(exampleObject.Key2);
 console.log(exampleObject["Key3"]);
@@ -34,3 +35,19 @@ var userObject = {
 }
 console.log("username is "+userObject.username);
 console.log("a/c num is "+userObject["account number"]); //with space in the key, I need this syntax
+*/
+
+function myFirstFunction(username) {
+//remember that object assignment has {}; i.e. ends in semi-colon
+//remember that function has {; i.e. does NOT have semi-colon
+    console.log("Hello");
+    console.log("My name is:");
+    console.log(username);
+}
+
+/*
+myFirstFunction("John");
+myFirstFunction("Waldo");
+myFirstFunction("Rumpelstiltskin");
+myFirstFunction("Carp");
+*/
