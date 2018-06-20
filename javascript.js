396bf373e6c1ed8a870d8b447b75f5f2ffac6a19
John Bateman <jbateman415@gmail.com>
2018-06-20 17:59:49 +0000

    completed and commented out WHILE and FOR loops; created object and accessed values of the K/V pairs in multiple ways

--- javascript.js
+++ javascript.js
@@ -1,5 +1,6 @@
 console.log("Executing javascript.js");
 
+/*
 var days = ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday", "Superday"];
 
 var arrayLength = days.length;
@@ -10,3 +11,26 @@ while (loopCounter < arrayLength) {
     console.log("Array Element "+ loopCounter + " is "+ days[loopCounter] );
     loopCounter++;
 }
+*/
+
+
+/*
+var fruits = ["apple","durian","banana","pineapple","tomato","peach"];
+console.log("Fruit Array length is "+fruits.length);
+
+for (var loopCounter2 = 0; loopCounter2 <fruits.length; loopCounter2++) { 
+    console.log("FruitArray Element "+ loopCounter2 + " is "+ fruits[loopCounter2] );
+}
+*/
+
+var exampleObject = {"Key1":1, "Key2":2, "Key3":3 };
+console.log(exampleObject.Key2);
+console.log(exampleObject["Key3"]);
+
+var userObject = {
+    "username": "jbateman",
+    "password": "myPassword",
+    "account number": 1234567890
+}
+console.log("username is "+userObject.username);
+console.log("a/c num is "+userObject["account number"]); //with space in the key, I need this syntax
